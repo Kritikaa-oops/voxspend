ad3808d7390c694f2b43fd3be8e99f43ebab0c97
reven <kandelritu897@gmail.com>
2026-07-06 13:17:05 +0545

    Add Expense struct definition

---
+++ expense.h
@@ -0,0 +1,27 @@
+#ifndef EXPENSE_H
+#define EXPENSE_H
+
+#include <string>
+
+using namespace std;
+
+struct Expense {
+    int id;
+    string category;
+    double amount;
+    string date;
+    string description;
+};
+
+struct Budget {
+    string category;
+    double limit;
+};
+
+struct User {
+    string email;
+    string password;
+    string name;
+};
+
+#endif
